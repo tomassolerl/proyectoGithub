cd7c8ffb26e99b7889c549c1690ee2e80bcc0a61
Tomás Soler <tomas@correo.com>
2019-11-28 12:10:22 +0100

    En veredicto, añadir comentarios

--- src/proyectogithub/ProyectoGithub.java
+++ src/proyectogithub/ProyectoGithub.java
@@ -12,13 +12,13 @@ package proyectogithub;
 public class ProyectoGithub {
 
     private static String veredicto(String valor1, String valor2){
-        if (valor1.equals(valor2)) {
+        if (valor1.equals(valor2)) { //Si el valor 1 es igual al valor 2 que devuelva la siguiente frase.
             return "Perfecto. Has tenido buen criterio.";
         }
-        if (Integer.valueOf(valor1) < Integer.valueOf(valor2)){
+        if (Integer.valueOf(valor1) < Integer.valueOf(valor2)){ //Si el valor 1 es mayor al valor 2 que devuelva la siguiente frase.
             return "Has mejorado, o te ha llegado el éxito inesperado.";
         }
-        return "Te has confiado. Falta realismo.";
+        return "Te has confiado. Falta realismo."; //Si no ha devuelto ninguna de las anteriores, devuelve esta última frase.
     }
     public static void main(String[] args) {
         System.out.println("La calificación de " + args[0] + ": ");
